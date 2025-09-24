7e586cbb79510716d9f4399f8b296baba2896aab
Oscar Chau <oscarchau126@gmail.com>
2025-09-24 11:19:14 -0700

    Fixed text

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
@@ -34,7 +37,11 @@ fun GradScreen(modifier: Modifier = Modifier) {
     Text(
         text = "Graduation Announcement",
         fontSize = 50.sp,
-        modifier = modifier
+        modifier = modifier,
+        lineHeight = 50.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center
     )
 }
 
